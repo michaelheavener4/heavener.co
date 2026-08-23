20d9ef177b83d7e553e410ee6e4a58de5ec8d28f
michaelheavener4 <heavenermichael4@gmail.com>
2026-08-23 14:05:59 -0700

    Serve GitHub page at github subdomain root

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,10 +1,11 @@
 export async function onRequest(context) {
-  const url = new URL(context.request.url);
+  const { request, next, env } = context;
+  const url = new URL(request.url);
 
   if (url.hostname === 'github.heavener.co' && url.pathname === '/') {
-    url.pathname = '/github/';
-    return context.env.ASSETS.fetch(url);
+    const githubUrl = new URL('/github/', request.url);
+    return env.ASSETS.fetch(new Request(githubUrl, request));
   }
 
-  return context.next();
+  return next();
 }
